4e57efdccc0153095ab15c8c51114501cbc8a8ff
shivsh94 <shivamsharma23jan@gmail.com>
2024-05-13 19:36:00 +0530

    Added random button

--- cngBgbutton/chaiaurcode.js
+++ cngBgbutton/chaiaurcode.js
@@ -1,6 +1,16 @@
 const button = document.querySelectorAll('.button')
 const body = document.querySelector("body")
 
+const randomColor = function(){
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+    for(let i = 0; i < 6; i++){
+       color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+
+};
+
 button.forEach(function(button){
     console.log(button);
     button.addEventListener('click', function(e){
@@ -16,8 +26,8 @@ button.forEach(function(button){
         if(e.target.id==='blue'){
             body.style.backgroundColor = e.target.id;
         }
-        if(e.target.id==='yellow'){
-            body.style.backgroundColor = e.target.id;
+        if(e.target.id==='Random'){
+            body.style.backgroundColor = randomColor();
         }
     });   
     
